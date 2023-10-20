ab1a25ebc432acd9b46508e5200ec326e591fec1
ysf <email.baissiysf@gmail.com>
2023-10-20 14:22:27 -0700

    ysf - third commit

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* yt - including libraries*/
+#include <unistd.h>
+#include <limits.h>
+#include <stdarg.h>
+
+/* yt - functions prototype */
+int _printf(const char *format, ...);
+int yt_putchar(char c);
+int yt_print_int(int i);
+
+#endif
